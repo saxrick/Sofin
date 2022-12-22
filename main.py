b7e32fcc2e5eb8deb398e0e4224dab450ca3196d
saxrick <artemsofin12@gmail.com>
2022-12-23 01:28:12 +0500

    aboba

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ class Dataset:
     @staticmethod
     def check_input(vacancy):
         if vacancy == 'qwertyuiop':
-            return 'abobaboababoa'
+            return ''
 
     @staticmethod
     def prepare_csv(file_name, vacancy):
